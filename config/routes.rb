2cc5f65f1a6bd9f9a85f9bd887dfa433cdb22400
Erin Scherl <rinrun413@gmail.com>
2021-02-12 11:12:44 -0600

    Added footer

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/new'
   get '/about', to: 'static_pages#about'
   get '/contact', to: "static_pages#contact"
   get '/random', to: "static_pages#random"
+  get '/signup', to: "users#new"
  root 'static_pages#home'
 end

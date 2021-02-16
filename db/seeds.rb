5c4da6537fa78cfd9419cb82d739fbd351d450bb
Erin Scherl <rinrun413@gmail.com>
2021-02-16 15:47:09 -0600

    Seed fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create([{name: 'Erin Scherl', email: 'scherler@grinnell.edu', password:'J0hn@nderson', password_confirmation'J0hn@nderson'},
-            {name: 'Test User', email: 'test@email.com', password:'123456', password_confirmation'123456'}])
+User.create([{name: 'Erin Scherl', email: 'scherler@grinnell.edu', password:'J0hn@nderson', password_confirmation:'J0hn@nderson'},
+            {name: 'Test User', email: 'test@email.com', password:'123456', password_confirmation:'123456'}])

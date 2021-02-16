ca86041f7d08eccd9abe6f029f6b13ee0c520a99
Erin Scherl <rinrun413@gmail.com>
2021-02-16 15:34:12 -0600

    HW6

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
   get '/about', to: 'static_pages#about'
   get '/contact', to: "static_pages#contact"
   get '/random', to: "static_pages#random"
   get '/signup', to: "users#new"
- root 'static_pages#home'
+  resources :users
 end
